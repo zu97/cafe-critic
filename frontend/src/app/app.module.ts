1cd57a242cbf583be23f10319b3ff3996835f954
admin <admin@admin.com>
2022-07-02 18:57:39 +0600

    Creation of ui components, centered card, loader and input file for frontend work

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppStoreModule } from './app-store.module';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { CenteredCardComponent } from './ui/centered-card/centered-card.component';
+import { FileInputComponent } from './ui/file-input/file-input.component';
+import { LoaderComponent } from './ui/loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
+    CenteredCardComponent,
+    FileInputComponent,
+    LoaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,6 +28,9 @@ import { PageNotFoundComponent } from './page-not-found.component';
     FlexLayoutModule,
     AppStoreModule,
     AppRoutingModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
