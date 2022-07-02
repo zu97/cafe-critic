c68a958bd3116b4b2e380f5667364c4c54502930
admin <admin@admin.com>
2022-07-02 18:50:26 +0600

    Frontend project initialization. Installing and configuring the store and router. Preparing the test environment

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing.module';
+import { AppStoreModule } from './app-store.module';
+
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppStoreModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ frontend/src/app/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-not-found',
+  template: `<h3  class="not-found">Page not found!</h3>`,
+  styles: ['.not-found { margin: 0; text-align: center; font-weight: bold; }']
+})
+export class PageNotFoundComponent {}
